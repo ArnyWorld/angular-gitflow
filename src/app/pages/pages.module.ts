49a26f5462d5fa5cd565f9d47f89bf98d631408d
Arnaldo Muñoz <munozarni12@gmail.com>
2023-10-06 16:18:26 -0400

    products page created

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductsComponent
+  ],
   imports: [
     CommonModule,
     PagesRoutingModule
